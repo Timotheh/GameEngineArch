eda09d640014c332f3e2347df9399c6d1c3254b3
Thomas Howe <howethomas98@gmail.com>
2017-11-27 10:18:17 +0000

    Assingment

---
+++ CIT2213-Assesment/Assets/GameIdea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameIdea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //guild of dungeoring style, choose dungeon before from 10 prefab tiles and then choose 5 mobs and 1 end point
+        //level up after each fight which add 1 health 	
+	}
+}
---
+++ CIT2213-Assesment/Assets/LevelSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawn : MonoBehaviour {
+    public GameObject LevelTile;
+	// Use this for initialization
+	void Start ()
+    {
+        for (int x = 0; x <= 10; x++)
+        {
+            for(int z = 0; z <=10; z++)
+            {
+                Instantiate(LevelTile, new Vector3(LevelTile.transform.localPosition.x + x, LevelTile.transform.position.y, LevelTile.transform.position.z + z), Quaternion.identity);
+            }
+        }
+        Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
